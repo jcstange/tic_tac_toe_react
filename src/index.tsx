3abb1bbdab07b5f8a101de963838f4b6d257a2de
jcstange <jcstange@gmail.com>
2021-07-23 13:53:25 +0300

    Improving UI

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,17 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
+const Colors = {
+    black: '#000000',
+    white: '#FFFFFF',
+    green: '#00FF00',
+    red: '#FF0000',
+    blue: '#0000FF',
+    light_green: '#52C7A2',
+    light_red: '#FF9999',
+    light_blue: '#9999FF',
+    gray: '#AAAAAA'
+} 
 
 type IBoard = {
     squares: string[],
@@ -9,6 +20,52 @@ type IBoard = {
 }
 
 const Board: React.FC = () => {
+    const styles = {
+        resetButton: {
+            fontSize: 25,
+            padding: 15,
+            marginTop: 30,
+            color: Colors.white,
+            backgroundColor: Colors.light_green,
+            borderWidth: 1,
+            borderRadius: 5,
+            borderColor: Colors.white,
+            fontFamily: 'Roboto',
+            marginLeft: '33%',
+            width: '33%',
+            marginRight: '33%',
+        },
+        status: {
+            fontSize: 25,
+            fontFamily: 'Roboto',
+            padding: 30,
+            marginBottom: 25, 
+            justifyContent: 'center',
+            color: Colors.white,
+            backgroundColor: Colors.gray
+        },
+        boardRow: {
+            display: 'flex'
+        },
+        board: {
+            display: 'block',
+            width: '90%',
+            marginLeft: '5%',
+            marginRight: '5%',
+            backgroundColor: Colors.gray,
+        },
+        vDivider: {
+            backgroundColor: Colors.gray,
+            width: 6,
+            height: '100%',
+        },
+        hDivider: {
+            backgroundColor: Colors.gray,
+            width: '100%',
+            height: 4,
+        }
+    }
+
     const [ values, setAllValues ] = useState<IBoard>({
         squares: Array(9).fill("-"),
         xIsNext: true 
@@ -17,6 +74,7 @@ const Board: React.FC = () => {
     function handleClick(square: number) {
         if(didGameEnd()) return
         const newSquares = values.squares.slice()
+        if(newSquares[square] !== "-") return
         newSquares[square] = values.xIsNext ? 'X' : 'O'
         setAllValues({...values, squares: newSquares, xIsNext: !values.xIsNext})
     }
@@ -72,23 +130,33 @@ const Board: React.FC = () => {
 
     return (
         <div>
-            <div className="status">{getStatusString()}</div>
-            <div className="board-row">
-                {renderSquare(0)}
-                {renderSquare(1)}
-                {renderSquare(2)}
+            <div className="status" style={styles.status}>{getStatusString()}</div>
+            <div className="board" style={styles.board}>
+                <div className="board-row" style={styles.boardRow}>
+                    {renderSquare(0)}
+                    <div style={styles.vDivider}/>
+                    {renderSquare(1)}
+                    <div style={styles.vDivider}/>
+                    {renderSquare(2)}
+                </div>
+                {<div style={styles.hDivider}/>}
+                <div className="board-row" style={styles.boardRow}>
+                    {renderSquare(3)}
+                    <div style={styles.vDivider}/>
+                    {renderSquare(4)}
+                    <div style={styles.vDivider}/>
+                    {renderSquare(5)}
+                </div>
+                {<div style={styles.hDivider}/>}
+                <div className="board-row" style={styles.boardRow}>
+                    {renderSquare(6)}
+                    <div style={styles.vDivider}/>
+                    {renderSquare(7)}
+                    <div style={styles.vDivider}/>
+                    {renderSquare(8)}
+                </div>
             </div>
-            <div className="board-row">
-                {renderSquare(3)}
-                {renderSquare(4)}
-                {renderSquare(5)}
-            </div>
-            <div className="board-row">
-                {renderSquare(6)}
-                {renderSquare(7)}
-                {renderSquare(8)}
-            </div>
-            <button onClick={()=> reset()}>RESET</button>
+            <button style={styles.resetButton} onClick={()=> reset()}>RESET</button>
         </div>
     )
 }
@@ -100,18 +168,32 @@ type SquareProps = {
 }
 
 const Square: React.FC<SquareProps> = ( { value, onClick }) => {
+    const styles = {
+        square: {
+            width: '50%',
+            paddingBottom: '10%',
+            paddingTop: '10%',
+            margin: 0,
+            fontSize: 50,
+            justifyContent: 'center',
+            color: Colors.light_green,
+            backgroundColor: Colors.white,
+            border: 0,
+        }
+    }
+
     return (
     <button
       className="square"
-      style={{ width:60, height:60, margin:0 }}
+      style={styles.square}
       onClick={() => onClick()}
     >{value}</button>
     )
 }
 
 const Game: React.FC = () => 
-    <div className="game">
-        <div className="game-board">
+    <div className="game" style={{height:'100%', width: '100%'}}>
+        <div className="game-board" style={{height:'100%', width: '100%'}}>
             <Board />
         </div>
         <div className="game-info">
